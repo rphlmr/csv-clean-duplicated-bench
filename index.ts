4785d4627fe77fc12603dc31a62ed9e20d9a7925
rphlmr <raphael@uaerom.com>
2022-12-13 12:18:48 +0100

    add split in chunks + zip

--- index.ts
+++ index.ts
@@ -1,49 +1,32 @@
 import fs from "node:fs";
 import csvParser from "csv-parser";
 import languageEncoding from "detect-file-encoding-and-language";
+import Zip from "adm-zip";
 
 type Product = {
-  EAN: string;
+  ean: string;
+  storeTrigram: string;
 };
+
+const outputFolder = "./output";
+const chunksFolder = `${outputFolder}/chunks`;
 
 function parseWithMap(array: Product[]) {
   console.time("parseWithMap");
 
   const result: Product[] = [
-    ...new Map(array.map((item) => [item.EAN, item])).values(),
+    ...new Map(
+      array.map((item) => [
+        `${item.ean.trim()}${item.storeTrigram.trim()}`.toLowerCase(),
+        item,
+      ])
+    ).values(),
   ];
 
   console.log("parseWithMap count", result.length);
+  console.log("number of products", array.length);
+  console.log("number of duplicates", array.length - result.length);
   console.timeEnd("parseWithMap");
-
-  return result;
-}
-
-function parseWithReduce(array: Product[]) {
-  console.time("parseWithReduce");
-
-  const result: Product[] = Object.values(
-    array.reduce((acc, current) => ({ ...acc, [current.EAN]: current }), {})
-  );
-
-  console.log("parseWithReduce count", result.length);
-  console.timeEnd("parseWithReduce");
-
-  return result;
-}
-
-function parseWithReduceRight(array: Product[]) {
-  console.time("parseWithReduceRight");
-
-  const result: Product[] = Object.values(
-    array.reduceRight(
-      (acc, current) => ({ ...acc, [current.EAN]: current }),
-      {}
-    )
-  );
-
-  console.log("parseWithReduceRight count", result.length);
-  console.timeEnd("parseWithReduceRight");
 
   return result;
 }
@@ -62,7 +45,9 @@ async function removeDuplicates(parser: (array: Product[]) => Product[]) {
       .pipe(
         csvParser({
           separator: ";",
-          mapHeaders: ({ header }) => header.trim(),
+          mapHeaders: function ({ header }) {
+            return header.trim();
+          },
         })
       )
       .on("data", (data: Product) => {
@@ -80,11 +65,49 @@ async function removeDuplicates(parser: (array: Product[]) => Product[]) {
   return productsParsed;
 }
 
+// create chunked files from array
+async function createChunkedFiles(array: Product[]) {
+  const chunkSize = 10_000;
+  const chunks: Product[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+
+  if (fs.existsSync(outputFolder)) {
+    await fs.promises.rm(outputFolder, { recursive: true });
+  }
+
+  await fs.promises.mkdir(chunksFolder, { recursive: true });
+
+  const chunkPromises = chunks.map((chunk, index) => {
+    fs.promises.writeFile(
+      `${chunksFolder}/chunk-${index + 1}.json`,
+      JSON.stringify(chunk)
+    );
+  });
+
+  await Promise.all(chunkPromises);
+}
+
+async function zipFiles() {
+  const zip = new Zip();
+  zip.addLocalFolder(chunksFolder);
+  zip.writeZip(`${outputFolder}/products.zip`);
+}
+
 async function main() {
-  // CSV with 21044 rows
-  await removeDuplicates(parseWithMap); // best 5.958ms
-  await removeDuplicates(parseWithReduce); // 1:36.498 (m:ss.mmm) bad
-  await removeDuplicates(parseWithReduceRight); // 1:38.053 (m:ss.mmm) worst
+  console.log("I'm starting parsing the file and removing duplicates 🔥");
+  const payload = await removeDuplicates(parseWithMap);
+
+  console.log(
+    "That was cool, now I'm creating chunked files 🚀 in ./output/chunks"
+  );
+  await createChunkedFiles(payload);
+
+  zipFiles();
+
+  console.log("Done! 🎉");
 }
 
 main();
@@ -102,3 +125,32 @@ parseWithReduceRight count 21044
 parseWithReduceRight: 1:38.053 (m:ss.mmm)
 csv parse: 1:38.408 (m:ss.mmm)
 */
+
+// function parseWithReduce(array: Product[]) {
+//   console.time("parseWithReduce");
+
+//   const result: Product[] = Object.values(
+//     array.reduce((acc, current) => ({ ...acc, [current.ean]: current }), {})
+//   );
+
+//   console.log("parseWithReduce count", result.length);
+//   console.timeEnd("parseWithReduce");
+
+//   return result;
+// }
+
+// function parseWithReduceRight(array: Product[]) {
+//   console.time("parseWithReduceRight");
+
+//   const result: Product[] = Object.values(
+//     array.reduceRight(
+//       (acc, current) => ({ ...acc, [current.ean]: current }),
+//       {}
+//     )
+//   );
+
+//   console.log("parseWithReduceRight count", result.length);
+//   console.timeEnd("parseWithReduceRight");
+
+//   return result;
+// }
